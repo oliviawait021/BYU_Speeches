256ab018d030ca11eead175f25c92d8b325a587c
jackrod13 <120751056+jackrod13@users.noreply.github.com>
2025-03-12 10:03:54 -0600

    update

--- backend/BYUSpeechesApp/BYUSpeechesApp/obj/Debug/net8.0/BYUSpeechesApp.AssemblyInfo.cs
+++ backend/BYUSpeechesApp/BYUSpeechesApp/obj/Debug/net8.0/BYUSpeechesApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BYUSpeechesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99e8237b20aa74fb23539c340c9ff35a6f7d24c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0ea3a34c08646e43b2a8d0fb67afe390d21688")]
 [assembly: System.Reflection.AssemblyProductAttribute("BYUSpeechesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BYUSpeechesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
