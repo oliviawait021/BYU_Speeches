c3559c4e289d06df23fd895136e24260751ee000
jackrod13 <120751056+jackrod13@users.noreply.github.com>
2025-03-24 10:20:06 -0600

    merge conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Header from "./Header";
 import CalendarPage from "./CalendarPage";
 import HomePage from "./HomePage";
