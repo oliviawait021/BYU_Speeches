8c167006c4e833aa5c1dc3c503d91c02a5e0cde7
jackrod13 <120751056+jackrod13@users.noreply.github.com>
2025-03-24 06:58:27 -0600

    Syncing With Main

--- backend/BYUSpeechesApp/BYUSpeechesApp/obj/Debug/net8.0/BYUSpeechesApp.AssemblyInfo.cs
+++ backend/BYUSpeechesApp/BYUSpeechesApp/obj/Debug/net8.0/BYUSpeechesApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BYUSpeechesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67e4ce78c3b056e88203d67312155465a8bbab54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95bed7d3f205a9ef5352435055e2a65905688af3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BYUSpeechesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BYUSpeechesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
