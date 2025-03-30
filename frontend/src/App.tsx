68af2d1bca6452683a79305e45e46807866dba5e
Izzy Turner <izzyturner.2802@gmail.com>
2025-03-30 09:26:32 -0700

    profile page updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import CalendarPage from "./pages/CalendarPage";
 import HomePage from "./pages/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BYUSpeech from "./pages/speech";
 import Topics from "./pages/Topics";
+import ProfilePage from "./ProfilePage";
 
 export default function App() {
   const [currentView, setCurrentView] = useState("home");
   const [searchQuery, setSearchQuery] = useState("");
-
 
   const handleSearch = (query: string) => {
     setSearchQuery(query);
@@ -23,9 +23,9 @@ export default function App() {
         <Route path={"/calendar"} element={<CalendarPage />} />
         <Route path={"/speech"} element={<BYUSpeech />} />
         <Route path={"/topic"} element={<Topics />} />
-        <Route path={'/search'} element={<div>search!</div>} />
-        <Route path={"/profile"} element={<div>profile!</div>} />
-        <Route  path={"/login"} element={<div>login!</div>} />
+        <Route path={"/search"} element={<div>search!</div>} />
+        <Route path={"/profile"} element={<ProfilePage />} />
+        <Route path={"/login"} element={<div>login!</div>} />
       </Routes>
     </Router>
   );
